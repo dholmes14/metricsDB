ee13b83e3477dd018713da10f7313ffac57d08bb
dholmes14 <david.holmes14@nhs.net>
2021-03-13 15:52:48 +0000

    push from different location

---
+++ MetricsDB/DB/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    path('', views.Homepage, name='Homepage'),
+    path('variants/<int:variant_id>/', views.Variantpage, name='Variantpage'),
+    path('datainput/', views.Datainputpage, name='Datainputpage'),
+    path('bulkinput/', views.Bulkinputpage, name='Bulkinputpage'),
+]
