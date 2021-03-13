e5539c0e8f4968446f162832ed31d8fa83ead2ff
dholmes14 <david.holmes14@nhs.net>
2021-03-13 15:57:23 +0000

    removed old folder layout

--- VariantDatabase/DB/tables.py
+++
@@ -1,6 +0,0 @@
-import django_tables2 as tables
-
-class VariantTable(tables.Table):
-    class Meta:
-        model = Variant_data
-        sequence = (variant_cdna, variant_protein, variant_genome)
